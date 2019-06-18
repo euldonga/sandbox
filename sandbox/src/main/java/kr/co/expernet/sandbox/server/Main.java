1dc7c5b2e73819fffcd3a8c08f323378b52731a7
Kei <euldonga@gmail.com>
2019-06-18 14:49:12 +0900

    파일 리시브 구현
    Signed-off-by: Kei <euldonga@gmail.com>

--- sandbox/src/main/java/kr/co/expernet/sandbox/server/Main.java
+++ sandbox/src/main/java/kr/co/expernet/sandbox/server/Main.java
@@ -6,26 +6,13 @@ import org.slf4j.LoggerFactory;
 import kr.co.expernet.sandbox.server.connection.ConnectionRunnable;
 import kr.co.expernet.sandbox.server.receiver.CcReceiver;
 import kr.co.expernet.sandbox.server.receiver.GcsReceiver;
-import kr.co.expernet.sandbox.server.stream.CcStreamer;
-import kr.co.expernet.sandbox.server.stream.GcsStreamer;
 
 public class Main {
 	private static final Logger log = LoggerFactory.getLogger(Main.class);
 	
 	public static void main(String[] args) {
-		log.info("-----------------------< START MAVLINK RELAY SERVER >-----------------------");
-		new Thread(new ConnectionRunnable(CcReceiver.class, 21000)).start();
-		new Thread(new ConnectionRunnable(GcsReceiver.class, 22000)).start();
-		new Thread(new ConnectionRunnable(CcReceiver.class, 21001)).start();
-		new Thread(new ConnectionRunnable(GcsReceiver.class, 22001)).start();
-		new Thread(new ConnectionRunnable(CcReceiver.class, 21002)).start();
-		new Thread(new ConnectionRunnable(GcsReceiver.class, 22002)).start();
-		log.info("-----------------------< START VIDEO STREAMING SERVER >-----------------------");
-		new Thread(new ConnectionRunnable(CcStreamer.class, 41000)).start();
-		new Thread(new ConnectionRunnable(GcsStreamer.class, 42000)).start();
-		new Thread(new ConnectionRunnable(CcStreamer.class, 41001)).start();
-		new Thread(new ConnectionRunnable(GcsStreamer.class, 42001)).start();
-		new Thread(new ConnectionRunnable(CcStreamer.class, 41002)).start();
-		new Thread(new ConnectionRunnable(GcsStreamer.class, 42002)).start();
+		log.info("-----------------------< START AUDIO RELAY SERVER >-----------------------");
+		new Thread(new ConnectionRunnable(CcReceiver.class, 51000)).start();
+		new Thread(new ConnectionRunnable(GcsReceiver.class, 52000)).start();
 	}
 }
--- sandbox/src/main/java/kr/co/expernet/sandbox/server/receiver/CcReceiver.java
+++ sandbox/src/main/java/kr/co/expernet/sandbox/server/receiver/CcReceiver.java
@@ -2,8 +2,12 @@ package kr.co.expernet.sandbox.server.receiver;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
-import java.io.IOException;
+import java.io.File;
 import java.net.Socket;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,8 +15,6 @@ import org.slf4j.LoggerFactory;
 import kr.co.expernet.sandbox.server.enums.Client;
 import kr.co.expernet.sandbox.server.handler.IOHandler;
 import kr.co.expernet.sandbox.server.mapper.CcMapper;
-import kr.co.expernet.sandbox.server.mapper.GcsMapper;
-import kr.co.expernet.sandbox.server.protocol.Mavlink;
 
 public class CcReceiver implements Runnable {
 	private static final Logger log = LoggerFactory.getLogger(CcReceiver.class);
@@ -30,7 +32,7 @@ public class CcReceiver implements Runnable {
 	@Override
 	public void run() {
 		log.info("[{}] --- [{}] CONNECTED.", Thread.currentThread().getName(), key);
-		relayPacket();
+		sendFile();
 	}
 	
 	private void init(Socket socket) {
@@ -42,28 +44,26 @@ public class CcReceiver implements Runnable {
 			e.printStackTrace();
 			IOHandler.close(bis, bos);
 		}
+		
 	}
 	
-	private void relayPacket() {
+	private void sendFile() {
+		AudioInputStream ais = null;
+		AudioInputStream pcm = null;
 		try {
-			int len = 0;
-			byte[] buffer = new byte[Mavlink.SIZE];
-			while ((len = bis.read(buffer, 0, buffer.length)) != -1) {
-				BufferedOutputStream gcs = GcsMapper.getBos(Client.GCS.getName());
-				if (gcs != null) {
-					gcs.write(buffer, 0, len);
-					gcs.flush();
-				}
+			ais = AudioSystem.getAudioInputStream(new File("/expernet/audio/tts.mp3"));
+			pcm = AudioSystem.getAudioInputStream(AudioFormat.Encoding.PCM_SIGNED, ais);
+			int readCount = 0;
+			byte[] buffer = new byte[1024];
+			while ((readCount = pcm.read(buffer)) != -1) {
+				bos.write(buffer, 0, readCount);
+				bos.flush();
 			}
-		} catch (IOException e) {
+			
+		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
-			try {
-				IOHandler.close(CcMapper.class, key, bis, bos);
-				log.info("[{}] --- [{}] TERMINATE.", Thread.currentThread().getName(), key);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
+			IOHandler.close(ais, pcm, bis, bos);
 		}
 	}
 }
